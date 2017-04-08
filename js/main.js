b742e83c4d01a2cd35d4b739808742f0f314dbe3
Kari Gilbertson <kari.a.gilbertson@gmail.com>
2017-04-08 14:33:16 -0400

    Create transition of pages

--- js/main.js
+++ js/main.js
@@ -46,10 +46,9 @@ function tomatoesFade() {
   	});
 }
 
-$(document).ready(function() {
-   baconFade();
-
-  const handle = $( "#serving-custom-handle" );
+function sliders() {
+  const handle = $("#custom-handle");
+  
   $("#serving-slider-range").slider({
     range: true,
     min: 2,
@@ -61,7 +60,6 @@ $(document).ready(function() {
     }
   });
 
-  // const handle = $( "#custom-handle" );
   $("#prep-slider-range").slider({
     range: true,
     min: 0,
@@ -73,7 +71,6 @@ $(document).ready(function() {
     }
   });
 
-  // const handle = $( "#custom-handle" );
   $("#cooking-slider-range").slider({
     range: true,
     min: 0,
@@ -84,5 +81,75 @@ $(document).ready(function() {
       $("#cooking-right-mins").text(`${ui.values[1]} mins`);
     }
   });
+}
 
-});
+function transitions() {
+  $(".link").fastClick(function () {
+    screen = "#" + $(this).attr("page-load");
+    if($(this).hasClass("none")){
+      $(".sc").removeClass("selected");
+      $(screen).addClass("selected");
+    }
+    if($(this).hasClass("slideup")){
+      $(".selected").addClass("previous");
+      $(screen).css({y:$(window).height() + "px"}).addClass("selected");
+      $(screen).transition({y:"0px"},300,"ease",function() {
+        $(".previous").removeClass("selected");
+        $(".previous").removeClass("previous");
+        $(".sc").removeAttr("style");
+      }); 
+    }
+    if($(this).hasClass("slidedown")){
+      $(".selected").addClass("previous");
+      $(screen).addClass("selected");
+      $(screen).css({y:"-" + $(window).height() + "px"});
+      $(screen).transition({y:"0px"},300,"ease",function() {
+        $(".previous").removeClass("selected");
+        $(".previous").removeClass("previous");
+        $(".sc").removeAttr("style");
+      }); 
+    }
+    if($(this).hasClass("slideleft")){
+      $(".selected").addClass("previous");
+      $(screen).css({x:$(window).width() + "px"}).addClass("selected");
+      $(".previous").transition({x:"-" + $(window).width() + "px"},300,"ease");
+      $(screen).transition({x:"0px"},300,"ease",function () {
+        $(".previous").removeClass("selected");
+        $(".previous").removeClass("previous");
+        $(".sc").removeAttr("style");
+      }); 
+      
+    }
+    if($(this).hasClass("slideright")){
+      $(".selected").addClass("previous");
+      $(screen).css({x:"-" + $(window).width() + "px"}).addClass("selected");
+      $(".previous").transition({x:$(window).width() + "px"},300,"ease");
+      $(screen).transition({x:"0px"},300,"ease",function () {
+        $(".previous").removeClass("selected");
+        $(".previous").removeClass("previous");
+        $(".sc").removeAttr("style");
+      }); 
+      
+    }
+    if($(this).hasClass("backdown")){
+      $(".selected").addClass("previous");
+      $(screen).show();
+      $(".previous").css("z-index","24").transition({y:$(window).height() + "px"},300,"ease",function() {
+        $(".previous").removeClass("selected");
+        $(".previous").removeClass("previous");
+        $(screen).addClass("selected");
+        $(".sc").removeAttr("style")
+      }); 
+    }
+    if($(this).hasClass("backup")){
+      $(".selected").addClass("previous");
+      $(screen).show();
+      $(".previous").css("z-index","24").transition({y: "-" + $(window).height() + "px"},300,"ease",function() {
+        $(".previous").removeClass("selected");
+        $(".previous").removeClass("previous");
+        $(screen).addClass("selected");
+        $(".sc").removeAttr("style");
+      }); 
+    }
+  });
+}
